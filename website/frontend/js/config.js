e87b70db95627058090bb4a0d741d2463c4d9a76
g4vkzc7h5k-tech <e7032413@gmail.com>
2026-08-31 23:24:38 +0200

    Update config.js

--- website/frontend/js/config.js
+++ website/frontend/js/config.js
@@ -1,6 +1,6 @@
 // Point this at wherever your backend is deployed - see deployment
 // notes for how to get a public URL for it.
-const API_BASE = "http://localhost:8000";
+const API_BASE = "https://blaid.onrender.com";
 
 const BLADE_LINKS = {
   invite: "https://discord.com/oauth2/authorize?client_id=1499768350773874830&permissions=8&integration_type=0&scope=bot+applications.commands",
